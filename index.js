f66216b5440c763ed048eb2ee38d92111416d463
Rachel Wyatt <rw126@evansville.edu>
2019-04-08 14:40:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var customerName = "bob"
 
 function upperCaseCustomerName(name) {
-  var customerName = name.toUpperCase(name)
+  var customerName = name.toUpperCase()
 }
 
 function setBestCustomer() {
